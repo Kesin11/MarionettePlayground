c880317e14cca0deaf8ef73f323f36199d30bfeb
Kenta Kase <kesin1202000@gmail.com>
2017-04-09 14:41:23 +0900

    Modelイベントと再render

--- src/main.js
+++ src/main.js
@@ -1,47 +1,7 @@
-import Bb from 'backbone'
-import Mn from 'backbone.marionette'
+import { ParentView, CounterModel } from './modules/counter'
 
-const date = {count: 0}
+const data = {count: 0}
 
-const ChildView = Mn.View.extend({
-  template: "#child-tmpl",
-  // triggersは直接イベントを発火させる。イベントの引数にはchildViewが付く
-  triggers: {
-    "click #count-up": "count:up"
-  },
-  // eventsはメソッドを呼び出す。引数は自分で決められる
-  events: {
-    "click #count-down": "countDown"
-  },
-  countDown: function() {
-    console.log("count down")
-    this.triggerMethod("child count:down", 1)
-  }
-})
-
-const ParentView = Mn.View.extend({
-  el: "#app",
-  template: "#header",
-  regions: {
-    childRegion: "#child-region"
-  },
-  onRender() {
-    this.showChildView("childRegion", new ChildView())
-  },
-  // 明示的にChildViewのイベントをハンドリング
-  // 代わりにonChildview**というメソッドを実装していけば直接ハンドリングされる
-  childViewEvents: {
-    "count:up": "onCountUp",
-    "count:down": "onCountDown",
-  },
-  onCountUp: function(childView) {
-    console.log("parent onChildviewCountUp", childView)
-  },
-  onCountDown: function(data) {
-    console.log("parent onChildviewCountDown", data)
-  },
-})
-
-const model = new Bb.Model(date)
-const view = new ParentView({model: model})
+const counterModel = new CounterModel(data)
+const view = new ParentView({model: counterModel})
 view.render()
---
+++ src/modules/counter.js
@@ -0,0 +1,56 @@
+import Bb from 'backbone'
+import Mn from 'backbone.marionette'
+
+const ChildView = Mn.View.extend({
+  template: "#child-tmpl",
+  // triggersは直接イベントを発火させる。イベントの引数にはchildViewが付く
+  triggers: {
+    "click #count-up": "count:up"
+  },
+  // eventsはメソッドを呼び出す。引数は自分で決められる
+  events: {
+    "click #count-down": "countDown"
+  },
+  countDown: function() {
+    this.triggerMethod("child count:down", 2)
+  }
+})
+
+export const ParentView = Mn.View.extend({
+  el: "#app",
+  template: "#header",
+  regions: {
+    childRegion: "#child-region"
+  },
+  // modelのイベントをハンドリング
+  modelEvents: {
+    "change": "onModelChange"
+  },
+  onModelChange: function() {
+    this.render()
+  },
+  onRender() {
+    this.showChildView("childRegion", new ChildView())
+  },
+  // 明示的にChildViewのイベントをハンドリング
+  // 代わりにonChildview**というメソッドを実装していけば直接ハンドリングされる
+  childViewEvents: {
+    "count:up": "onCountUp",
+    "count:down": "onCountDown",
+  },
+  onCountUp: function(_childView) {
+    this.model.increment()
+  },
+  onCountDown: function(num) {
+    this.model.decrement(num)
+  },
+})
+
+export const CounterModel = Bb.Model.extend({
+  increment: function() {
+    this.set('count', this.get('count') + 1)
+  },
+  decrement: function(num) {
+    this.set('count', this.get('count') - num)
+  }
+})
