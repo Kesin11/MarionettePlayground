f958724d29b6115b1e0ea5e35c8358a1198232aa
Kenta Kase <kesin1202000@gmail.com>
2017-04-09 14:22:06 +0900

    子要素とイベントの伝搬

--- src/main.js
+++ src/main.js
@@ -1,14 +1,47 @@
 import Bb from 'backbone'
 import Mn from 'backbone.marionette'
 
-const date = {name: 'foo', message: 'bar'}
+const date = {count: 0}
 
-const View = Mn.View.extend({
+const ChildView = Mn.View.extend({
+  template: "#child-tmpl",
+  // triggersは直接イベントを発火させる。イベントの引数にはchildViewが付く
+  triggers: {
+    "click #count-up": "count:up"
+  },
+  // eventsはメソッドを呼び出す。引数は自分で決められる
+  events: {
+    "click #count-down": "countDown"
+  },
+  countDown: function() {
+    console.log("count down")
+    this.triggerMethod("child count:down", 1)
+  }
+})
+
+const ParentView = Mn.View.extend({
   el: "#app",
-  template: "#header"
+  template: "#header",
+  regions: {
+    childRegion: "#child-region"
+  },
+  onRender() {
+    this.showChildView("childRegion", new ChildView())
+  },
+  // 明示的にChildViewのイベントをハンドリング
+  // 代わりにonChildview**というメソッドを実装していけば直接ハンドリングされる
+  childViewEvents: {
+    "count:up": "onCountUp",
+    "count:down": "onCountDown",
+  },
+  onCountUp: function(childView) {
+    console.log("parent onChildviewCountUp", childView)
+  },
+  onCountDown: function(data) {
+    console.log("parent onChildviewCountDown", data)
+  },
 })
 
 const model = new Bb.Model(date)
-console.log(model)
-const view = new View({model: model})
+const view = new ParentView({model: model})
 view.render()
