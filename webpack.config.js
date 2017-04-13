193c0a4fea612c0109fa8812bb84c306b1f5106f
Kenta Kase <kesin1202000@gmail.com>
2017-04-14 00:08:03 +0900

    main.js -> app.jsにリネーム

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
