2df312115e9bc5e3b828f885308acd6390c3ea00
Kenta Kase <kesin1202000@gmail.com>
2017-04-17 00:51:54 +0900

    scssのwebpackビルド環境を構築

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
+module.exports = [{
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -32,7 +33,28 @@ module.exports = {
     hints: false,
   },
   devtool: '#eval-source-map',
-}
+},{
+  entry: './src/app.scss',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'styles.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", 'sass-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ],
+}]
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
